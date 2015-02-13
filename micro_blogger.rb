b872ed8c99915ee813637b4313648a9a874e284b
Collin Smith <collink33@gmail.com>
2015-02-13 12:07:33 -0800

    add twitter client

--- micro_blogger.rb
+++ micro_blogger.rb
@@ -1,4 +1,6 @@
 require 'jumpstart_auth'
+require 'bitly'
+Bitly.use_api_version_3
 
 class MicroBlogger
   attr_reader :client
@@ -28,14 +30,57 @@ class MicroBlogger
       when 'q' then puts "Goodbye!"
       when 't' then tweet(parts[1..-1].join(" "))
       when 'dm' then dm(parts[1], parts[2..-1].join(" "))
+      when 'smf' then spam_my_followers(parts[1..-1].join(" "))
+      when 'elt' then everyones_last_tweet
+      when 's' then shorten(parts[1])
+      when 'turl' then tweet(parts[1..-2].join(" ") + " " + shorten(parts[-1]))
       else puts "Sorry, I don't know how to #{command}"
       end
     end
   end
 
   def dm(target, message)
+    screen_names = @client.followers.collect { |follower| @client.user(follower).screen_name }
     puts "Trying to send #{target} this direct message:"
-    puts message
+    if screen_names.include?(target)
+      puts message
+      message = "d @#{target} #{message}"
+      tweet(message)
+    else
+      puts "Error: You can only direct message people who are following you."
+    end
+  end
+
+  def followers_list
+    screen_names = []
+    @client.followers.each do |follower|
+      screen_names << @client.user(follower).screen_name
+    end
+    screen_names
+  end
+
+  def spam_my_followers(message)
+    followers_list.each { |follower| dm(follower, message) }
+  end
+
+  def everyones_last_tweet
+    friends = @client.friends
+
+    # There's a problem here. 'friend' is a FixNum, so calling
+    # 'status' and 'screen_name' raises an error.
+    friends.each do |friend|
+      last_message = friend.status.text 
+      print "#{friend.screen_name} said..."
+      print last_message
+      puts ''
+    end
+  end
+
+  def shorten(original_url)
+    puts "Shortening this URL: #{original_url}"
+    bitly = Bitly.new('hungryacademy', 'R_430e9f62250186d2612cca76eee2dbc6')
+    return bitly.shorten(original_url).short_url
+    
   end
 end
 
