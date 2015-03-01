4fd5d22020d979c09a21312da920afbbf4300aba
Collin Smith <collink33@gmail.com>
2015-03-01 14:34:26 -0800

    add knight - initialize and check_move

---
+++ lib/chess/knight.rb
@@ -0,0 +1,28 @@
+class Knight < Piece
+  def initialize(position, board, color)
+    @type = "N"
+    super(position, board, color)
+  end
+
+  def check_move(new_position)
+    super(new_position)
+    new_column, new_row, old_column, old_row = columns_and_rows(new_position)
+
+    allowed_moves = [[old_column + 2, old_row + 1],
+                     [old_column + 2, old_row - 1],
+                     [old_column + 1, old_row + 2],
+                     [old_column + 1, old_row - 2],
+                     [old_column - 1, old_row + 2],
+                     [old_column - 1, old_row - 2],
+                     [old_column - 2, old_row + 1],
+                     [old_column - 2, old_row - 1]]
+    
+    # Return true if the new_position is an allowed move.
+    # Otherwise, return false.
+    if allowed_moves.include?(new_position)
+      return true
+    else
+      raise ArgumentError, "Invalid move."
+    end
+  end
+end
