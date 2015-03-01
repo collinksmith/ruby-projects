761a939678ae23f5c6871d1444a2d200c3da1301
Collin Smith <collink33@gmail.com>
2015-03-01 14:33:25 -0800

    add tests for check_move

--- spec/rook_spec.rb
+++ spec/rook_spec.rb
@@ -1,1 +1,23 @@
 require_relative 'spec_helper'
+
+describe Rook do
+  before :each do
+    @board = Board.new
+    @rook = Rook.new([4,4], @board, :white)
+  end
+
+  context ".check_move" do
+    it "can move along a column" do
+      expect(@rook.check_move([4,7])).to eq(true)
+    end
+
+    it "can move along a row" do
+      expect(@rook.check_move([3,4])).to eq(true)
+    end
+
+    it "cannot move diagonally" do
+      expect { @rook.check_move([5,5]) }.to raise_error(ArgumentError)
+    end
+
+  end
+end
