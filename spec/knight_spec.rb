088859d8e184a932f94ba75dc81c1b0b08263947
Collin Smith <collink33@gmail.com>
2015-03-01 14:34:08 -0800

    add tests for check_move

--- spec/knight_spec.rb
+++ spec/knight_spec.rb
@@ -1,1 +1,33 @@
 require_relative 'spec_helper'
+
+describe Knight do
+  before :each do
+    @board = Board.new
+    @knight = Knight.new([4,4], @board, :white)
+  end
+
+  describe ".check_move" do
+    it "can make all it's correct moves" do
+      expect(@knight.check_move([6,5])).to eq(true)
+      expect(@knight.check_move([6,3])).to eq(true)
+      expect(@knight.check_move([5,6])).to eq(true)
+      expect(@knight.check_move([5,2])).to eq(true)
+      expect(@knight.check_move([3,6])).to eq(true)
+      expect(@knight.check_move([3,2])).to eq(true)
+      expect(@knight.check_move([2,5])).to eq(true)
+      expect(@knight.check_move([2,3])).to eq(true)
+    end
+
+    it "cannot move along a column" do
+      expect { @knight.check_move([6,4]) }.to raise_error(ArgumentError)
+    end
+
+    it "cannot move along a row" do
+      expect { @knight.check_move([4,6]) }.to raise_error(ArgumentError)
+    end
+
+    it "cannot move diagonally" do
+      expect { @knight.check_move([5,5]) }.to raise_error(ArgumentError)
+    end
+  end
+end
