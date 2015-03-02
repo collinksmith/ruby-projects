2194aa43dcceff16bae0711b4058ae52607be0b5
Collin Smith <collink33@gmail.com>
2015-03-01 18:20:04 -0800

    add check?

--- lib/chess/game.rb
+++ lib/chess/game.rb
@@ -1,7 +1,33 @@
 class Game
+  attr_accessor :board, :white_player, :black_player
   def initialize
     @board = Board.new
     @white_player = Player.new(@board, :white)
     @black_player = Player.new(@board, :black)
+  end
+
+  # Return true if the given player is in check.
+  def check?(color)
+    if color == :white
+      player = @white_player
+      other_player = @black_player
+    else
+      player = @black_player
+      other_player = @white_player
+    end
+
+    king_position = player.king.position
+
+    other_player.pieces.each do |piece|
+      begin
+        return true if piece.check_move(king_position)
+      rescue
+      end
+    end
+    return false
+  end
+
+  def checkmate?(player)
+
   end
 end
