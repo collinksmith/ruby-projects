8b334922969a4ce527a6ad909b97f37b48966ae5
Collin Smith <collink33@gmail.com>
2015-03-01 20:54:41 -0800

    fix check_move
    - Add more specific error messages
    - Fix check of black pawns' home row.

--- lib/chess/pawn.rb
+++ lib/chess/pawn.rb
@@ -12,7 +12,7 @@ class Pawn < Piece
 
     # Raise an error if the move is straight ahead and the spot is occupied by any piece
     if new_column == old_column && @board.cells[new_column][new_row].piece
-      raise ArgumentError, "Invalid move."
+      raise ArgumentError, "Invalid move. Pawns cannot capture straight ahead."
     end
 
     if @color == :white
@@ -24,19 +24,22 @@ class Pawn < Piece
         if piece_at_new_position && piece_at_new_position.color == :black
           return true
         else
-          raise ArgumentError, "Invalid move."
+          raise ArgumentError, "Invalid move. Pawns can only move diagonally if capturing a piece."
         end
 
       # Otherwise, the pawn can never switch columns
       else
-        return false unless new_column = old_column
+        unless new_column == old_column
+          raise ArgumentError, "Invalid move. Cannot switch columns unless capturing a piece."
+          return false
+        end
       end
 
       # Pawn can move forward 2 if on home row. Otherwise, can only move forward 1
       if old_row == 1
-        raise ArgumentError, "Invalid move." unless new_row == old_row + 1 || new_row == old_row + 2
+        raise ArgumentError, "Invalid move. When on home row, pawns can only move 1 or 2 sqares ahead." unless new_row == old_row + 1 || new_row == old_row + 2
       else
-        raise ArgumentError, "Invalid move." unless new_row == old_row + 1
+        raise ArgumentError, "Invalid move. When not on home row, pawns can only move one square ahead." unless new_row == old_row + 1
       end
     end
 
@@ -49,19 +52,22 @@ class Pawn < Piece
         if piece_at_new_position && piece_at_new_position.color == :white
           return true
         else
-          raise ArgumentError, "Invalid move."
+          raise ArgumentError, "Invalid move. Pawns can only move diagonally if capturing a piece."
         end
 
       # Otherwise, the pawn can never switch columns
       else
-        return false unless new_column = old_column
+        unless new_column == old_column
+          raise ArgumentError, "Invalid move. Cannot switch columns unless capturing a piece."
+          return false
+        end
       end
 
       # Pawn can move forward 2 if on home row. Otherwise, can only move forward 1
-      if old_row == 7
-        raise ArgumentError, "Invalid move." unless new_row == old_row - 1 || new_row == old_row - 2
+      if old_row == 6
+        raise ArgumentError, "Invalid move. When on home row, pawns can only move 1 or 2 sqares ahead." unless new_row == old_row - 1 || new_row == old_row - 2
       else
-        raise ArgumentError, "Invalid move." unless new_row == old_row - 1
+        raise ArgumentError, "Invalid move. When not on home row, pawns can only move one square ahead." unless new_row == old_row - 1
       end
     end
 
