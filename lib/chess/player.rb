47c80b646b22bdcb6fd57bc3c31e6bf3f476b867
Collin Smith <collink33@gmail.com>
2015-03-01 16:15:17 -0800

    update move
    - Add functionality to take an opposing piece.
    - This deletes the piece from the opposing player's 'pieces' array

--- lib/chess/player.rb
+++ lib/chess/player.rb
@@ -1,4 +1,5 @@
 class Player
+  attr_accessor :pieces
   def initialize(board, color)
     # puts color
     if color == :white
@@ -10,38 +11,43 @@ class Player
     end
     @color = color
     @board = board
-    @pieces = [Rook.new([0,0 + piece_adder], board, color),
-               Knight.new([1, 0 + piece_adder], board, color),
-               Bishop.new([2, 0 + piece_adder], board, color),
-               Queen.new([3, 0 + piece_adder], board, color),
-               King.new([4, 0 + piece_adder], board, color),
-               Bishop.new([5, 0 + piece_adder], board, color),
-               Knight.new([6, 0 + piece_adder], board, color),
-               Rook.new([7, 0 + piece_adder], board, color),
-               Pawn.new([0, 1 + pawn_adder], board, color),
-               Pawn.new([1, 1 + pawn_adder], board, color),
-               Pawn.new([2, 1 + pawn_adder], board, color),
-               Pawn.new([3, 1 + pawn_adder], board, color),
-               Pawn.new([4, 1 + pawn_adder], board, color),
-               Pawn.new([5, 1 + pawn_adder], board, color),
-               Pawn.new([6, 1 + pawn_adder], board, color),
-               Pawn.new([7, 1 + pawn_adder], board, color),]
+    @pieces = [Rook.new([0,0 + piece_adder], board, color, self),
+               Knight.new([1, 0 + piece_adder], board, color, self),
+               Bishop.new([2, 0 + piece_adder], board, color, self),
+               Queen.new([3, 0 + piece_adder], board, color, self),
+               King.new([4, 0 + piece_adder], board, color, self),
+               Bishop.new([5, 0 + piece_adder], board, color, self),
+               Knight.new([6, 0 + piece_adder], board, color, self),
+               Rook.new([7, 0 + piece_adder], board, color, self),
+               Pawn.new([0, 1 + pawn_adder], board, color, self),
+               Pawn.new([1, 1 + pawn_adder], board, color, self),
+               Pawn.new([2, 1 + pawn_adder], board, color, self),
+               Pawn.new([3, 1 + pawn_adder], board, color, self),
+               Pawn.new([4, 1 + pawn_adder], board, color, self),
+               Pawn.new([5, 1 + pawn_adder], board, color, self),
+               Pawn.new([6, 1 + pawn_adder], board, color, self),
+               Pawn.new([7, 1 + pawn_adder], board, color, self),]
 
   end
 
-  # Check if the player has a piece on the old position. If not raise an error.
-  # Then, check if the move is legal.
-  # Then, set the position of the piece to the new position
+  # If legal, move the piece to the new position.
+  # If there's an opposing piece there, delete it.
   def move(old_position, new_position)
     old_column, old_row = old_position[0], old_position[1]
     new_column, new_row = new_position[0], new_position[1]
     piece = @board.get_piece([old_column, old_row])
 
-    unless piece.color == @color
+    # Check that 
+    if piece == nil || piece.color != @color
       raise ArgumentError, "You don't have a piece on that square."
     end
 
     piece.check_move(new_position)
+
+    if @board.get_piece([new_column, new_row])
+      @board.get_piece([new_column, new_row]).delete
+    end
+
     piece.set_position(new_position)
   end
 end
