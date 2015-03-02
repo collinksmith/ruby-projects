7f88b431521621fcb5ae47e30040d0b794a06af2
Collin Smith <collink33@gmail.com>
2015-03-01 16:13:55 -0800

    add tests for move

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -31,9 +31,6 @@ describe Player do
 
   describe ".move" do
     context "when making a legal move" do
-      before :all do
-        # @white_player.move([1,1], [1,3])
-      end
       it "sets the piece on the old cell to nil" do
         @white_player.move([1,1], [1,3])
         expect(@board.get_piece([1,1])).to eq(nil)
@@ -51,5 +48,26 @@ describe Player do
       end
     end
 
+    context "when trying to move a piece that doesn't exist" do
+      it "raises an error" do
+        expect { @white_player.move([3,3], [4,4]) }.to raise_error(ArgumentError)
+      end
+    end
+
+    context "when trying to move an opposig piece" do
+      it "raises an error" do
+        expect { @white_player.move([0,6]) }.to raise_error(ArgumentError)
+      end
+    end
+
+    context "when taking an opposing piece" do
+      it "removes a piece from the opposing player" do
+        white_piece = Rook.new([4,4], @board, :white, @white_player)
+        expect(@black_player.pieces.length).to eq(16)
+        @white_player.move([4,4], [4,6])
+        expect(@black_player.pieces.length).to eq(15)
+      end
+
+    end
   end
 end
