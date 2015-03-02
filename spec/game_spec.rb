424fd6f156463386b128dabad560fd845ac0ecde
Collin Smith <collink33@gmail.com>
2015-03-01 18:20:14 -0800

    add test for check?

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,1 +1,19 @@
 require_relative 'spec_helper.rb'
+
+describe Game do
+  before :each do
+    @game = Game.new
+  end
+
+  context "when player is in check" do
+    describe ".check?" do
+      it "returns true" do
+        @game.white_player.move([4,1], [4,2])
+        @game.black_player.move([5,6], [5,5])
+        @game.white_player.move([3,0], [7,4])
+        expect(@game.check?(:black)).to eq(true)
+      end
+    end
+  end
+
+end
