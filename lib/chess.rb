b6f75a367f23da88d1c3e240d8d78cb70689947a
Collin Smith <collink33@gmail.com>
2015-03-02 20:44:36 -0800

    add game logic
    - Add convert_position. Converts a human-readable position (e.g. a1) into a computer readable position (e.g. [0,0])
    - Add get_move. Asks the player for their move and returns it in a form that can be passed to the move functions.
    - Add player_turn.
    - Add check_status. Checks to see if a player is in check or checkmate. If in check, simply states that. If in checkmate, sets a game_over flag to true.
    - Add play_game. Uses the previous functions to go all the way through a game.

--- lib/chess.rb
+++ lib/chess.rb
@@ -20,3 +20,95 @@ appropriate cell as the computer understands it (in this example, [4,7]).
 game logic in this file, implementing saving, and the unicode characters for pieces.
 
 =end
+
+# Take a position as entered by a human, and convert it
+# to something the computer understands
+
+require_relative 'chess.rb'
+require_relative 'chess/bishop.rb'
+require_relative 'chess/board.rb'
+require_relative 'chess/cell.rb'
+require_relative 'chess/game.rb'
+require_relative 'chess/helper.rb'
+require_relative 'chess/king.rb'
+require_relative 'chess/knight.rb'
+require_relative 'chess/pawn.rb'
+require_relative 'chess/piece.rb'
+require_relative 'chess/player.rb'
+require_relative 'chess/queen.rb'
+require_relative 'chess/rook.rb'
+
+def convert_position(position)
+  letter_to_number = {'a' => 0,
+                      'b' => 1,
+                      'c' => 2,
+                      'd' => 3,
+                      'e' => 4,
+                      'f' => 5,
+                      'g' => 6,
+                      'h' => 7}
+
+  column = letter_to_number[position[0]]
+  row = position[1].to_i
+
+  return [column, row-1]
+end
+
+def get_move(player_color)
+  player = player_color == :white ? 'White player' : 'Black player'
+  puts "#{player}, what is your move?"
+  move = gets.chomp.split
+  move.map! { |position| position.split('') }
+  move = [convert_position(move[0]), convert_position(move[1])]
+  return move
+end
+
+def player_turn(game, player)
+  begin
+    move = get_move(player.color)
+    player.move(move[0], move[1])
+    if game.check?(player.color)
+      player.move(move[1], move[0])
+      raise ArgumentError, "That move puts you in check!"
+    end
+  rescue ArgumentError => e
+    print e
+    puts "Please try again."
+    retry
+  end
+end
+
+def check_status(game, player)
+  other_player = player.color == :black ? 'White player' : "Black player"
+  game_over = false
+  if game.check?(player.color)
+    if game.checkmate?(player.color)
+      game_over = true
+      puts "Checkmate. #{other_player} wins!"
+    else
+      puts "Check."
+    end
+  end
+  game_over
+end
+
+def play_game
+  game = Game.new
+  game_over = false
+
+  counter = 0
+  until game_over
+    if counter.even?
+      game.board.display
+      player_turn(game, game.white_player)
+      game_over = check_status(game, game.black_player)
+    else
+      game.board.display
+      player_turn(game, game.black_player)
+      game_over = check_status(game, game.white_player)
+    end
+    counter += 1
+  end
+end
+
+play_game
