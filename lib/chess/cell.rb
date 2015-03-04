aea812a98b9de1546d48246971d5349a0bb11a76
Collin Smith <collink33@gmail.com>
2015-03-03 18:11:03 -0800

    add unicode chess symbols

--- lib/chess/cell.rb
+++ lib/chess/cell.rb
@@ -14,7 +14,29 @@ class Cell
     end
   end
 
+  def get_chess_symbol(color, status)
+    if color == :black
+      symbols = {'K' => "\u2654",
+                 'Q' => "\u2655",
+                 'R' => "\u2656",
+                 'B' => "\u2657",
+                 'N' => "\u2658",
+                 'P' => "\u2659"}
+    end
+
+    if color == :white
+      symbols = {'K' => "\u265A",
+                 'Q' => "\u265B",
+                 'R' => "\u265C",
+                 'B' => "\u265D",
+                 'N' => "\u265E",
+                 'P' => "\u265F"}
+    end
+
+    symbol = symbols[status]
+  end
+
   def to_s
-    @status
+    @status == ' ' ? @status : get_chess_symbol(@piece.color, @status)
   end
 end
