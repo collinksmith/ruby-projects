f7dffa5efbcd43e7953872f470a01f2b2d4c43c4
Collin Smith <collink33@gmail.com>
2015-03-05 21:54:27 -0800

    Add en passant
    - Add en_passant? method that checks if en passant is allowed for the current move.
    
    - Add move method
      1) Sets the game's en_passant flag to self if the pawn is moving two spaces forward from its home row
      2) Sets the game's en_passant flag to nil if the pawn is not moving two spaces forward from its home row
      3) Deletes the piece in the game's en_passant flag if the pawn is capturing by en passant
    
    - Update check_move to check if en passant is allowed, and set the 'capturing_by_en_passant' flag to true if so
    
    - Update initialize to accept a 'game' argument

--- lib/chess/pawn.rb
+++ lib/chess/pawn.rb
@@ -1,9 +1,10 @@
 require_relative 'piece.rb'
 
 class Pawn < Piece
-  def initialize(position, board, color, player=nil)
+  def initialize(position, board, color, player=nil, game)
     @type = 'P'
-    super(position, board, color, @type, player)
+    @capturing_by_en_passant = false
+    super(position, board, color, @type, player, game)
   end
 
   def check_move(new_position)
@@ -19,6 +20,12 @@ class Pawn < Piece
       # Check if the move is diagonal
       if ((new_column == old_column + 1) || (new_column == old_column -1)) && (new_row == old_row + 1)
         
+        #Check if en passant is allowed
+        if en_passant?(new_column, new_row)
+          @capturing_by_en_passant = true
+          return true
+        end
+
         # If so, can only move if the square is occupied by an opposing piece
         piece_at_new_position = @board.cells[new_column][new_row].piece
         if piece_at_new_position && piece_at_new_position.color == :black
@@ -47,6 +54,13 @@ class Pawn < Piece
       # Check if the move is diagonal
       if ((new_column == old_column + 1) || (new_column == old_column -1)) && new_row == old_row - 1
         
+        # Check if en passant is allowed
+        if en_passant?(new_column, new_row)
+          @capturing_by_en_passant = true
+          return true
+        end
+
+
         # If so, can only move if the square is occupied by an opposing piece
         piece_at_new_position = @board.cells[new_column][new_row].piece
         if piece_at_new_position && piece_at_new_position.color == :white
@@ -76,5 +90,46 @@ class Pawn < Piece
     end
 
     true
+  end
+
+  def en_passant?(new_column, new_row)
+    if @color == :white && @game.en_passant
+      # En passant is allowed if moving to row 5 and a column
+      # in which black just advanced a pawn to row 4
+      if @game.en_passant.position[0] == new_column && new_row == 5
+        return true
+      end
+    elsif @color == :black && @game.en_passant
+      # En passant i allowed if moving to row 2 and a column
+      # in which white just advanced a pawn to row 3
+      if @game.en_passant.position[0] == new_column && new_row == 2
+        return true
+      end
+    else
+    end
+
+    false
+  end
+
+  def move(new_position)
+    old_position = @position
+
+    super(new_position, true)
+
+    # If performing en passant, delete the opposing pawn
+    if @capturing_by_en_passant
+      @game.en_passant.delete
+      @capturing_by_en_passant = false
+    end
+
+    # Flag the pawn as available to be taken by en passant  if it is
+    # moving two spaces forward from its home row
+    if @color == :white && old_position[1] == 1 && new_position[1] == 3
+      @game.en_passant = self
+    elsif @color == :black && old_position[1] == 6 && new_position[1] == 4
+      @game.en_passant = self
+    else
+      @game.en_passant = nil
+    end
   end
 end
