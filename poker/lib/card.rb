1cfc358cca54e81cfced4d5541e2314ee7ba4f90
Collin Smith <collink33@gmail.com>
2015-07-03 13:52:21 -0700

    add some Hand specs

--- poker/lib/card.rb
+++ poker/lib/card.rb
@@ -1,40 +1,25 @@
 class Card
   SUIT_STRINGS = {
-    :clubs    => "♣",
-    :diamonds => "♦",
-    :hearts   => "♥",
-    :spades   => "♠"
+    :clubs    => "♣", :diamonds => "♦",
+    :hearts   => "♥", :spades   => "♠"
   }
 
   VALUE_STRINGS = {
-    :deuce => "2",
-    :three => "3",
-    :four  => "4",
-    :five  => "5",
-    :six   => "6",
-    :seven => "7",
-    :eight => "8",
-    :nine  => "9",
-    :ten   => "10",
-    :jack  => "J",
-    :queen => "Q",
-    :king  => "K",
-    :ace   => "A"
+    :two => "2", :three => "3", :four  => "4", :five  => "5",
+    :six   => "6", :seven => "7", :eight => "8", :nine  => "9",
+    :ten   => "10", :jack  => "J", :queen => "Q", :king  => "K", :ace   => "A"
   }
 
   VALUE = {
-    :ace => 11,
-    :deuce => 2,
-    :three => 3,
-    :four  => 4,
-    :five  => 5,
-    :six   => 6,
-    :seven => 7,
-    :eight => 8,
-    :nine  => 9,
-    :ten   => 10,
-    :jack  => 10,
-    :queen => 10,
-    :king  => 10
+    :ace => 11, :two => 2, :three => 3, :four  => 4,
+    :five  => 5, :six   => 6, :seven => 7, :eight => 8,
+    :nine  => 9, :ten   => 10, :jack  => 10, :queen => 10, :king  => 10
   }
+
+  attr_reader :suit, :value
+
+  def initialize(value, suit)
+    @value = value
+    @suit = suit
+  end
 end
