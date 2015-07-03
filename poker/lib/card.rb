15f7746a70b2f6da0221daa9cce87bd9a2b56010
Collin Smith <collink33@gmail.com>
2015-07-03 13:15:51 -0700

    start poker tests

---
+++ poker/lib/card.rb
@@ -0,0 +1,40 @@
+class Card
+  SUIT_STRINGS = {
+    :clubs    => "♣",
+    :diamonds => "♦",
+    :hearts   => "♥",
+    :spades   => "♠"
+  }
+
+  VALUE_STRINGS = {
+    :deuce => "2",
+    :three => "3",
+    :four  => "4",
+    :five  => "5",
+    :six   => "6",
+    :seven => "7",
+    :eight => "8",
+    :nine  => "9",
+    :ten   => "10",
+    :jack  => "J",
+    :queen => "Q",
+    :king  => "K",
+    :ace   => "A"
+  }
+
+  VALUE = {
+    :ace => 11,
+    :deuce => 2,
+    :three => 3,
+    :four  => 4,
+    :five  => 5,
+    :six   => 6,
+    :seven => 7,
+    :eight => 8,
+    :nine  => 9,
+    :ten   => 10,
+    :jack  => 10,
+    :queen => 10,
+    :king  => 10
+  }
+end
